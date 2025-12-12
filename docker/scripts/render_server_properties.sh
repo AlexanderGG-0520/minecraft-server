77272ebecdbd43dfc43063fe78f0fae225b38b0c
AlexanderGG-0520 <uket.panda.1st@gmail.com>
2025-12-13 03:27:26 +0900

    refactor: Simplify player addition logic and improve UUID retrieval in render_server_properties.sh

--- docker/scripts/render_server_properties.sh
+++ docker/scripts/render_server_properties.sh
@@ -1,40 +1,25 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
-# プレイヤー名を環境変数から取得（OPSまたはWHITELIST）
-OPS=${OPS:-}
-WHITELIST=${WHITELIST:-}
-
-add_player_to_json() {
-    local player_name=$1
-    local json_file=$2
-
-    # Mojang APIを使ってプレイヤーのUUIDを取得
-    player_uuid=$(curl -s "https://api.mojang.com/users/profiles/minecraft/$player_name" | jq -r '.id')
-
-    if [ "$player_uuid" == "null" ]; then
-        echo "Error: Could not find UUID for player $player_name"
-        return 1
-    fi
-
-    # プレイヤーをops.jsonまたはwhitelist.jsonに追加
-    echo "Adding $player_name ($player_uuid) to $json_file"
-    
-    # ops.jsonまたはwhitelist.jsonにプレイヤーを追加
-    jq ". += [{\"uuid\": \"$player_uuid\", \"name\": \"$player_name\", \"level\": 4}]" "$json_file" > temp.json && mv temp.json "$json_file"
+# プレイヤー名からUUIDを取得するための関数
+get_uuid() {
+  local player_name=$1
+  # Minecraft Profile API から UUID を取得
+  uuid=$(curl -s "https://api.mojang.com/users/profiles/minecraft/${player_name}" | jq -r '.id')
+  echo $uuid
 }
 
-# OPSのプレイヤーをops.jsonに追加
-if [ -n "$OPS" ]; then
-    for player in $(echo "$OPS" | tr "," "\n"); do
-        add_player_to_json "$player" "/data/ops.json"
-    done
-fi
+# OPSの処理
+for player in $(echo "$OPS" | sed "s/,/ /g")
+do
+  uuid=$(get_uuid "$player")
+  echo "Adding player $player ($uuid) to /data/ops.json"
+  jq ". += [{\"uuid\": \"$uuid\", \"name\": \"$player\", \"level\": 4, \"bypassesPlayerLimit\": false}]" /data/ops.json > /data/ops.json.tmp && mv /data/ops.json.tmp /data/ops.json
+done
 
-# WHITELISTのプレイヤーをwhitelist.jsonに追加
-if [ -n "$WHITELIST" ]; then
-    for player in $(echo "$WHITELIST" | tr "," "\n"); do
-        add_player_to_json "$player" "/data/whitelist.json"
-    done
-fi
-
-echo "OPS and WHITELIST processing completed"
+# WHITELISTの処理
+for player in $(echo "$WHITELIST" | sed "s/,/ /g")
+do
+  uuid=$(get_uuid "$player")
+  echo "Adding player $player ($uuid) to /data/whitelist.json"
+  jq ". += [{\"uuid\": \"$uuid\", \"name\": \"$player\", \"bypassesPlayerLimit\": false}]" /data/whitelist.json > /data/whitelist.json.tmp && mv /data/whitelist.json.tmp /data/whitelist.json
+done
