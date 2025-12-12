2eb49868589c942947e75520f21cb738fa935a21
AlexanderGG-0520 <uket.panda.1st@gmail.com>
2025-12-13 03:36:52 +0900

    fix: Improve logging for player processing in render_server_properties.sh

--- docker/scripts/render_server_properties.sh
+++ docker/scripts/render_server_properties.sh
@@ -11,15 +11,21 @@ get_uuid() {
 # OPSの処理
 for player in $(echo "$OPS" | sed "s/,/ /g")
 do
+  echo "Processing player: $player"
   uuid=$(get_uuid "$player")
-  echo "Adding player $player ($uuid) to /data/ops.json"
+  echo "UUID for player $player: $uuid"
+  echo "Adding player $player to ops.json"
   jq ". += [{\"uuid\": \"$uuid\", \"name\": \"$player\", \"level\": 4, \"bypassesPlayerLimit\": false}]" /data/ops.json > /data/ops.json.tmp && mv /data/ops.json.tmp /data/ops.json
 done
+
 
 # WHITELISTの処理
 for player in $(echo "$WHITELIST" | sed "s/,/ /g")
 do
+  echo "Processing whitelist player: $player"
   uuid=$(get_uuid "$player")
-  echo "Adding player $player ($uuid) to /data/whitelist.json"
+  echo "UUID for player $player: $uuid"
+  echo "Adding player $player to whitelist.json"
   jq ". += [{\"uuid\": \"$uuid\", \"name\": \"$player\", \"bypassesPlayerLimit\": false}]" /data/whitelist.json > /data/whitelist.json.tmp && mv /data/whitelist.json.tmp /data/whitelist.json
 done
+
